4a3a09c59c0f8f48e9b623a05713d66aa17a7043
Richard Kooijman <richard.kooijman@inergy.nl>
2020-01-12 13:35:54 +0100

    added some action scripts

---
+++ climate_on.py
@@ -0,0 +1,15 @@
+import commands
+import keychain
+from teslapy import Tesla
+
+
+with Tesla(commands.ACCOUNT, keychain.get_password(commands.SERVICE, commands.ACCOUNT),
+           commands.CLIENT_ID, commands.CLIENT_SECRET) as tesla:
+    tesla.fetch_token()
+    cars = tesla.vehicle_list()
+    selected = [c for c in cars if c['id_s'] == keychain.get_password(commands.SERVICE, commands.VEHICLE_ID)]
+    if len(selected) != 1:
+        raise ValueError("one and only one car must be selected")
+    for i, vehicle in enumerate(selected):
+        vehicle.sync_wake_up()
+        vehicle.command('CLIMATE_ON')
